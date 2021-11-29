4a82bce3266a162f5bdb14f2bd334e69f0e9e1f9
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-11-29 16:27:18 +0900

    init

---
+++ pagelist/data/data.js
@@ -0,0 +1,60 @@
+const data = {}
+data.guide = {
+  title: 'Guide',
+  list: [
+    {
+      stateClass: 'complete',
+      url: 'color',
+      category: 'guide',
+      page: 'COLORS'
+    },
+    {
+      stateClass: 'complete',
+      url: 'table',
+      category: 'guide',
+      page: 'table'
+    },
+    {
+      stateClass: 'complete',
+      url: 'button',
+      category: 'guide',
+      page: 'button'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_01',
+      category: 'guide',
+      page: 'TYPOGRAPHY'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_02',
+      category: 'guide',
+      page: 'TYPOGRAPHY(한글)'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_03',
+      category: 'guide',
+      page: 'TYPOGRAPHY(영어)'
+    },
+    {
+      stateClass: 'complete',
+      url: 'grid',
+      category: 'guide',
+      page: 'grid'
+    },
+    {
+      stateClass: 'complete',
+      url: 'input',
+      category: 'guide',
+      page: 'INPUT'
+    },
+    {
+      stateClass: 'complete',
+      url: 'loading',
+      category: 'guide',
+      page: 'loading'
+    }
+  ]
+}
