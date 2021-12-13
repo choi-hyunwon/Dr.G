aa767155e601eaec01c38bb9f1e554c182314f8b
bcde77 <bcde77@domfam.com>
2021-12-13 23:47:38 +0900

    01의견남기기tab_02비회원의견남기기 (100%)

--- Dr.G-mobile/src/js/common.js
+++ Dr.G-mobile/src/js/common.js
@@ -1,6 +1,7 @@
 /**
  * --------------------------------------------------------------------------
  *  common.js
+ v.0.1 선미 : tab 추가 -- 2021.12.13
  * --------------------------------------------------------------------------
  */
 var front = front || {};
@@ -12,6 +13,7 @@ front.common = (function () {
     var init = function () {
         this.a();
         this.commonHandler();
+        this.tab();
     }
 
     var a = function () {
@@ -22,31 +24,45 @@ front.common = (function () {
 
     var commonHandler = function () {
         /* select */
-        $('.custom-select').on('change', function() {
+        $('.custom-select').on('change', function () {
             var value = $(this).children("option:selected").attr('value');
 
-            if($(this).not('&.footer-select')){
+            if ($(this).not('&.footer-select')) {
                 if (!value) {
                     $(this).css({
-                        'font-weight' : 'normal',
-                        'color' : '#666666',
-                        'background' : '#fff url(../../img/common/ico-select-disabled.png) right 20px center/20px 20px no-repeat'
+                        'font-weight': 'normal',
+                        'color': '#666666',
+                        'background': '#fff url(../../img/common/ico-select-disabled.png) right 20px center/20px 20px no-repeat'
                     });
                 } else {
                     $(this).css({
-                        'font-weight' : 'bold',
-                        'color' : '#444444',
-                        'background' : '#fff url(../../img/common/ico-select.png) right 20px center/20px 20px no-repeat'
+                        'font-weight': 'bold',
+                        'color': '#444444',
+                        'background': '#fff url(../../img/common/ico-select.png) right 20px center/20px 20px no-repeat'
                     });
                 }
             }
         });
     }
 
+    var tab = function () {
+        $('.tab .item a').on('click', function () {
+            let idx = $(this).parent().index()
+            let tabItem = $('.tab .item')
+            let tabPane = $('.tab-content .tab-pane')
+            console.log('a')
+            tabItem.removeClass('active')
+            tabItem.eq(idx).addClass('active')
+            tabPane.removeClass('active')
+            tabPane.eq(idx).addClass('active')
+        })
+    }
+
     return {
         a: a,
         commonHandler: commonHandler,
         init: init,
+        tab: tab
     }
 })();
 
