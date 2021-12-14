1d41da0009c1259108db1471d5a67d99f6c7dbf6
bcde77 <bcde77@domfam.com>
2021-12-14 14:45:32 +0900

    data.js 정리

--- Dr.G-mobile/pagelist/data/data.js
+++ Dr.G-mobile/pagelist/data/data.js
@@ -70,26 +70,40 @@ data.guide = {
     },
   ]
 }
-data.common = {
-  title: 'common',
+
+data.suggestion = {
+  title: '고객제안',
   list: [
     {
       stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
+      category: 'suggestion',
+      page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
+    },
+    {
+      stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
+      category: 'suggestion',
+      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
+    },
+    {
+      stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
+      category: 'suggestion',
+      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
+    },
+    {
+      stateClass: 'complete',
       url: '09고객제안_01의견남기기tab_유형선택',
-      category: 'common',
+      category: 'suggestion',
       page: '09고객제안_01의견남기기tab_유형선택'
     },
     {
       stateClass: 'complete',
       url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
-      category: 'common',
+      category: 'suggestion',
       page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
     },
-  ]
-}
-data.suggestion = {
-  title: '고객제안',
-  list: [
     {
       stateClass: 'complete',
       url: '09고객제안_01의견남기기tab_02비회원의견남기기',
@@ -101,30 +115,6 @@ data.suggestion = {
       url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)',
       category: 'suggestion',
       page: '01의견남기기tab_03개선사례조회_01상세보기(답변대기)'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
-    },
-    // {
-    //   stateClass: 'complete',
-    //   url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
-    //   category: 'common',
-    //   page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
-    // },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
     },
     {
       stateClass: 'complete',
