1384d549c1a8db5a68a5c968b7bbda820125bf4e
taeyoon <Gotaeyoon007@gmail.com>
2021-12-14 13:39:21 +0900

    data.js 수정완료

--- Dr.G-mobile/pagelist/data/data.js
+++ Dr.G-mobile/pagelist/data/data.js
@@ -75,27 +75,9 @@ data.common = {
     },
     {
       stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
-    },
-    {
-      stateClass: 'complete',
       url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
       category: 'common',
       page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
-      category: 'common',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
     },
   ]
 }
@@ -113,7 +95,31 @@ data.suggestion = {
       url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)',
       category: 'suggestion',
       page: '01의견남기기tab_03개선사례조회_01상세보기(답변대기)'
-    }
+    },
+    {
+      stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
+      category: 'common',
+      page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
+    },
+    // {
+    //   stateClass: 'complete',
+    //   url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
+    //   category: 'common',
+    //   page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
+    // },
+    {
+      stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
+      category: 'common',
+      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
+    },
+    {
+      stateClass: 'complete',
+      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
+      category: 'common',
+      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
+    },
 
   ]
 }
