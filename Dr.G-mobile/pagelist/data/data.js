402821de81a67b6055c4abc774852fbfa2c8f808
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-12-15 11:01:18 +0900

    * 의견남기기 tab (비회원) 완료 / 버튼 min-width 제거

--- Dr.G-mobile/pagelist/data/data.js
+++ Dr.G-mobile/pagelist/data/data.js
@@ -1,139 +1,151 @@
 const data = {}
 data.guide = {
-  title: 'Guide',
-  list: [
-    {
-      stateClass: 'complete',
-      url: 'button',
-      category: 'guide',
-      page: 'button'
-    },
-    {
-      stateClass: 'complete',
-      url: 'color',
-      category: 'guide',
-      page: 'COLORS'
-    },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'grid',
-    //   category: 'guide',
-    //   page: 'grid'
-    // },
-    {
-      stateClass: 'complete',
-      url: 'icons',
-      category: 'guide',
-      page: 'icons'
-    },
-    {
-      stateClass: 'complete',
-      url: 'input',
-      category: 'guide',
-      page: 'INPUT'
-    },
-    {
-      stateClass: 'complete',
-      url: 'loading',
-      category: 'guide',
-      page: 'loading'
-    },
-    {
-      stateClass: 'complete',
-      url: 'tab',
-      category: 'guide',
-      page: 'tab'
-    },
-    {
-      stateClass: 'complete',
-      url: 'typography',
-      category: 'guide',
-      page: 'TYPOGRAPHY'
-    },
-    {
-      stateClass: 'complete',
-      url: 'layout',
-      category: 'guide',
-      page: 'layout'
-    },
-    {
-      stateClass: 'complete',
-      url: 'modal',
-      category: 'guide',
-      page: 'modal'
-    },
-    {
-      stateClass: 'complete',
-      url: 'badge',
-      category: 'guide',
-      page: 'badge'
-    },
-  ]
+    title: 'Guide',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'button',
+            category: 'guide',
+            page: 'button'
+        },
+        {
+            stateClass: 'complete',
+            url: 'color',
+            category: 'guide',
+            page: 'COLORS'
+        },
+        // {
+        //   stateClass: 'complete',
+        //   url: 'grid',
+        //   category: 'guide',
+        //   page: 'grid'
+        // },
+        {
+            stateClass: 'complete',
+            url: 'icons',
+            category: 'guide',
+            page: 'icons'
+        },
+        {
+            stateClass: 'complete',
+            url: 'input',
+            category: 'guide',
+            page: 'INPUT'
+        },
+        {
+            stateClass: 'complete',
+            url: 'loading',
+            category: 'guide',
+            page: 'loading'
+        },
+        {
+            stateClass: 'complete',
+            url: 'tab',
+            category: 'guide',
+            page: 'tab'
+        },
+        {
+            stateClass: 'complete',
+            url: 'typography',
+            category: 'guide',
+            page: 'TYPOGRAPHY'
+        },
+        {
+            stateClass: 'complete',
+            url: 'layout',
+            category: 'guide',
+            page: 'layout'
+        },
+        {
+            stateClass: 'complete',
+            url: 'modal',
+            category: 'guide',
+            page: 'modal'
+        },
+        {
+            stateClass: 'complete',
+            url: 'badge',
+            category: 'guide',
+            page: 'badge'
+        },
+    ]
 }
 
 data.suggestion = {
-  title: '고객제안',
-  list: [
-      {
-          stateClass: 'complete',
-          url: '09고객제안_01의견남기기tab',
-          category: 'suggestion',
-          page: '09고객제안_01의견남기기tab'
-      },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_유형선택',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_유형선택'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_02비회원의견남기기',
-      category: 'suggestion',
-      page: '01의견남기기tab_02비회원의견남기기'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)',
-      category: 'suggestion',
-      page: '01의견남기기tab_03개선사례조회_01상세보기(답변대기)'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_02상세보기(답변완료)',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_02상세보기(답변완료)'
-    },
-    {
-      stateClass: 'complete',
-      url: '09고객제안_01의견남기기tab_03개선사례조회_03수정하기실행',
-      category: 'suggestion',
-      page: '09고객제안_01의견남기기tab_03개선사례조회_03수정하기실행'
-    }
+    title: '고객제안',
+    list: [
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_02나의제안내역tab(회원)',
+            category: 'suggestion',
+            page: '09고객제안_02나의제안내역tab(회원)'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_02나의제안내역tab(비회원)',
+            category: 'suggestion',
+            page: '09고객제안_02나의제안내역tab(비회원)'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_01비회원로그인알림팝업',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_01비회원로그인알림팝업'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)_알럿팝업_검색된내역없을시'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_유형선택',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_유형선택'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_등록완료알럿팝업',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_등록완료알럿팝업'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_02비회원의견남기기',
+            category: 'suggestion',
+            page: '01의견남기기tab_02비회원의견남기기'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_03개선사례조회_01상세보기(답변대기)',
+            category: 'suggestion',
+            page: '01의견남기기tab_03개선사례조회_01상세보기(답변대기)'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_03개선사례조회_02상세보기(답변완료)',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_03개선사례조회_02상세보기(답변완료)'
+        },
+        {
+            stateClass: 'complete',
+            url: '09고객제안_01의견남기기tab_03개선사례조회_03수정하기실행',
+            category: 'suggestion',
+            page: '09고객제안_01의견남기기tab_03개선사례조회_03수정하기실행'
+        }
 
-  ]
+    ]
 }
