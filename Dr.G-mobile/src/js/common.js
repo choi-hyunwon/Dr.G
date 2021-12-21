63db3ba4f5811c937cbb62ee57aeec7ada21ba78
taeyoon <Gotaeyoon007@gmail.com>
2021-12-21 17:55:24 +0900

    popup 모달 스크립트 전체 제거 완료 (공통으로 처리)

--- Dr.G-mobile/src/js/common.js
+++ Dr.G-mobile/src/js/common.js
@@ -4,6 +4,7 @@
  v.0.1 선미 : tab 추가 -- 2021.12.13
  * --------------------------------------------------------------------------
  */
+
 var front = front || {};
 
 front.common = front.common || {};
@@ -14,6 +15,13 @@ front.common = (function () {
         this.a();
         this.commonHandler();
         this.tab();
+        this.popupText();
+        this.changeText();
+        this.popupAge();
+        this.changeAge();
+        this.fullPopup();
+        this.defaultPopup();
+        this.introScroll();
     }
 
     var a = function () {
@@ -58,12 +66,139 @@ front.common = (function () {
             tabPane.eq(idx).addClass('active')
         })
     }
+    var defaultPopup = function defaultPopup(){
+        $('._toast-popup-open').click(function () {
+            $('._toast-dim-no-title').removeClass('hide').addClass('fade show').css('display','block');
+        });
+        $('._toast-popup-close').click(function () {
+            $('._toast-dim-no-title').addClass('hide');
+            setTimeout(() => {
+                $('._toast-dim-no-title').removeClass('fade show').css('display','none');
+            }, 400);
+        });
+    }
+
+    var popupText = function popup() {
+        $('._toast-popup-open.text').on('click', function () {
+            let select = $('._toast-popup.text')
+            select.removeClass('hide').addClass('fade show').css('display', 'block');
+        });
+    }
+    var changeText = function List() {
+        let list = $('._toast-popup.text .select-list > .list');
+
+        function hidePopup() {
+            $('._toast-popup.text').addClass('hide');
+            setTimeout(() => {
+                $('._toast-popup.text').removeClass('fade show').css('display', 'none');
+            }, 400);
+        }
+
+        /*클릭시 텍스트 변경*/
+        $(document).ready(function () {
+            list.on("click", function (e) {
+                let textValue = $(this).find('span').text();
+
+                function ChangeText() {
+                    $('._toast-popup-open.text').find('span').text(textValue).css({
+                        'color': '#444444',
+                        'font-weight': 'bold'
+                    });
+                }
+
+                if (list.hasClass("active")) {
+                    list.removeClass("active");
+                    $(this).addClass("active");
+                    hidePopup();
+                    ChangeText();
+                } else {
+                    $(this).addClass("active")
+                    hidePopup();
+                    ChangeText();
+                }
+            });
+        });
+    }
+    var popupAge = function popupAge(){
+        $('._toast-popup-open.age').on('click',function () {
+            $('._toast-popup.age')
+                .removeClass('hide')
+                .addClass('fade show')
+                .css('display', 'block');
+        });
+    }
+    var changeAge = function changeAge() {
+        let list = $('._toast-popup.age .select-list > .list');
+        function hidePopup() {
+            $('._toast-popup.age').addClass('hide');
+            setTimeout(() => {
+                $('._toast-popup.age').removeClass('fade show').css('display', 'none');
+            }, 400);
+        }
+        /*클릭시 텍스트 변경*/
+        $(document).ready(function () {
+            list.on("click", function (e) {
+                let textValue = $(this).find('span').text();
+
+                function ChangeText() {
+                    $('._toast-popup-open.age').find('span').text(textValue).css({
+                        'color': '#444444',
+                        'font-weight': 'bold'
+                    });
+                }
+
+                if (list.hasClass("active")) {
+                    list.removeClass("active");
+                    $(this).addClass("active");
+                    hidePopup();
+                    ChangeText();
+                } else {
+                    $(this).addClass("active")
+                    hidePopup();
+                    ChangeText();
+                }
+            });
+        });
+    }
+    var fullPopup = function fullPopup(){
+        $('._toast-popup-open.full').on('click', function () {
+            let select = $('._full-popup');
+            select.removeClass('hide').addClass('show');
+        });
+        $('._toast-popup-close.full').on('click', function () {
+
+            let select = $('._full-popup');
+            select.removeClass('show').addClass('hide');
+        });
+    }
+    var introScroll = function introScroll() {
+        var lastScroll = 0;
+        $('._full-popup > .popup-content').scroll(function () {
+            var st = $(this).scrollTop();
+            // console.log(st);
+            if (st === 0) {
+                // console.log('Down');
+                $('._popup-header').removeClass('header-white');
+            } else {
+                // console.log('Up');
+                $('._popup-header').addClass('header-white');
+            }
+            lastScroll = st;
+        });
+    }
 
     return {
         a: a,
+        defaultPopup : defaultPopup,
         commonHandler: commonHandler,
-        init: init,
+        popupText : popupText,
+        changeText : changeText,
+        changeAge : changeAge,
+        popupAge : popupAge,
+        fullPopup : fullPopup,
+        introScroll : introScroll,
         tab: tab,
+        init: init
     }
 })();
 
