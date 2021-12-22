0ef60fbcb54a74f8206d8f6be2c8383b552a8fd9
taeyoon <Gotaeyoon007@gmail.com>
2021-12-22 11:22:12 +0900

    스크립트 모듈화정리중

--- Dr.G-mobile/src/js/common.js
+++ Dr.G-mobile/src/js/common.js
@@ -3,6 +3,7 @@
  *  common.js
  v.0.1 선미 : tab 추가 -- 2021.12.13
  v.0.2 태윤 : 모달 관련 스크립트 추가 -- 2021.12.21
+ v.0.3 태윤 : 모달 리팩토링 -- 2021.12.22
  * --------------------------------------------------------------------------
  */
 
@@ -16,13 +17,9 @@ front.common = (function () {
         this.a();
         this.commonHandler();
         this.tab();
-        this.popupText();
-        this.changeText();
-        this.popupAge();
-        this.changeAge();
-        this.fullPopup();
-        this.defaultPopup();
+        this.popup();
         this.introScroll();
+        this.selectChange();
     }
 
     var a = function () {
@@ -33,23 +30,23 @@ front.common = (function () {
 
     var commonHandler = function () {
         /* header */
-        $('._header-sticky').sticky({topSpacing:0})
+        $('._header-sticky').sticky({topSpacing: 0})
 
         /* select */
-        $('.custom-select').each(function (){
-            $(this).on('change', function() {
+        $('.custom-select').each(function () {
+            $(this).on('change', function () {
                 var value = $(this).children("option:selected").attr('value');
                 if (!value) {
                     $(this).css({
-                        'font-weight' : 'normal',
-                        'color' : '#666666',
-                        'background-image' : 'url(../../img/common/ico-select-disabled.svg)'
+                        'font-weight': 'normal',
+                        'color': '#666666',
+                        'background-image': 'url(../../img/common/ico-select-disabled.svg)'
                     });
                 } else {
                     $(this).css({
-                        'font-weight' : 'bold',
-                        'color' : '#444444',
-                        'background-image' : 'url(../../img/common/ico-select.svg)'
+                        'font-weight': 'bold',
+                        'color': '#444444',
+                        'background-image': 'url(../../img/common/ico-select.svg)'
                     });
                 }
             });
@@ -67,111 +64,7 @@ front.common = (function () {
             tabPane.eq(idx).addClass('active')
         })
     }
-    var defaultPopup = function defaultPopup(){
-        $('._toast-popup-open').click(function () {
-            $('._toast-dim-no-title').removeClass('hide').addClass('fade show').css('display','block');
-        });
-        $('._toast-popup-close').click(function () {
-            $('._toast-dim-no-title').addClass('hide');
-            setTimeout(() => {
-                $('._toast-dim-no-title').removeClass('fade show').css('display','none');
-            }, 400);
-        });
-    }
 
-    var popupText = function popup() {
-        $('._toast-popup-open.text').on('click', function () {
-            let select = $('._toast-popup.text')
-            select.removeClass('hide').addClass('fade show').css('display', 'block');
-        });
-    }
-    var changeText = function List() {
-        let list = $('._toast-popup.text .select-list > .list');
-
-        function hidePopup() {
-            $('._toast-popup.text').addClass('hide');
-            setTimeout(() => {
-                $('._toast-popup.text').removeClass('fade show').css('display', 'none');
-            }, 400);
-        }
-
-        /*클릭시 텍스트 변경*/
-        $(document).ready(function () {
-            list.on("click", function (e) {
-                let textValue = $(this).find('span').text();
-
-                function ChangeText() {
-                    $('._toast-popup-open.text').find('span').text(textValue).css({
-                        'color': '#444444',
-                        'font-weight': 'bold'
-                    });
-                }
-
-                if (list.hasClass("active")) {
-                    list.removeClass("active");
-                    $(this).addClass("active");
-                    hidePopup();
-                    ChangeText();
-                } else {
-                    $(this).addClass("active")
-                    hidePopup();
-                    ChangeText();
-                }
-            });
-        });
-    }
-    var popupAge = function popupAge(){
-        $('._toast-popup-open.age').on('click',function () {
-            $('._toast-popup.age')
-                .removeClass('hide')
-                .addClass('fade show')
-                .css('display', 'block');
-        });
-    }
-    var changeAge = function changeAge() {
-        let list = $('._toast-popup.age .select-list > .list');
-        function hidePopup() {
-            $('._toast-popup.age').addClass('hide');
-            setTimeout(() => {
-                $('._toast-popup.age').removeClass('fade show').css('display', 'none');
-            }, 400);
-        }
-        /*클릭시 텍스트 변경*/
-        $(document).ready(function () {
-            list.on("click", function (e) {
-                let textValue = $(this).find('span').text();
-
-                function ChangeText() {
-                    $('._toast-popup-open.age').find('span').text(textValue).css({
-                        'color': '#444444',
-                        'font-weight': 'bold'
-                    });
-                }
-
-                if (list.hasClass("active")) {
-                    list.removeClass("active");
-                    $(this).addClass("active");
-                    hidePopup();
-                    ChangeText();
-                } else {
-                    $(this).addClass("active")
-                    hidePopup();
-                    ChangeText();
-                }
-            });
-        });
-    }
-    var fullPopup = function fullPopup(){
-        $('._toast-popup-open.full').on('click', function () {
-            let select = $('._full-popup');
-            select.removeClass('hide').addClass('show');
-        });
-        $('._toast-popup-close.full').on('click', function () {
-
-            let select = $('._full-popup');
-            select.removeClass('show').addClass('hide');
-        });
-    }
     var introScroll = function introScroll() {
         var lastScroll = 0;
         $('._full-popup > .popup-content').scroll(function () {
@@ -188,18 +81,99 @@ front.common = (function () {
         });
     }
 
+    var popup = function popup() {
+        let popupOpen = $('._toast-popup-open');
+        popupOpen.on('click', function () {
+                if ($(this).hasClass('text') === true) {
+                    let select = $('._toast-popup.text')
+                    select.removeClass('hide').addClass('fade show').css('display', 'block');
+                } else if ($(this).hasClass('age') === true) {
+                    let select = $('._toast-popup.age')
+                    select.removeClass('hide').addClass('fade show').css('display', 'block');
+                } else if ($(this).hasClass('full') === true) {
+                    let select = $('._full-popup');
+                    select.removeClass('hide').addClass('show');
+                }
+            }
+        )
+    }
+
+    var selectChange = function selectChange() {
+        let list = $('._toast-popup')
+        let selectValue = $('._toast-popup .select-list > .list');
+
+
+
+        if (list.hasClass('text') === true) {
+            $('._toast-popup').addClass('hide');
+
+            setTimeout(() => {
+                list.removeClass('fade show').css('display', 'none');
+            }, 400);
+            $(document).ready(function () {
+                selectValue.on("click", function (e) {
+                    let textValue = $(this).find('span').text();
+                    function ChangeText() {
+                        $('._toast-popup-open').find('span').text(textValue).css({
+                            'color': '#444444',
+                            'font-weight': 'bold'
+                        });
+                    }
+
+
+                    if (list.hasClass("active")) {
+                        list.removeClass("active");
+                        $(this).addClass("active");
+                        hidePopup();
+                        ChangeText();
+                    } else {
+                        $(this).addClass("active")
+                        hidePopup();
+                        ChangeText();
+                    }
+                });
+            });
+
+        } else if (list.hasClass('age') === true) {
+            $('._toast-popup').addClass('hide');
+            setTimeout(() => {
+                $('._toast-popup.age').removeClass('fade show').css('display', 'none');
+            }, 400);
+
+            $(document).ready(function () {
+                list.on("click", function (e) {
+                    let textValue = $(this).find('span').text();
+
+                    function ChangeText() {
+                        $('._toast-popup-open.age').find('span').text(textValue).css({
+                            'color': '#444444',
+                            'font-weight': 'bold'
+                        });
+                    }
+
+                    if (list.hasClass("active")) {
+                        list.removeClass("active");
+                        $(this).addClass("active");
+                        hidePopup();
+                        ChangeText();
+                    } else {
+                        $(this).addClass("active")
+                        hidePopup();
+                        ChangeText();
+                    }
+                });
+            });
+        }
+    }
+
     return {
         a: a,
-        defaultPopup : defaultPopup,
         commonHandler: commonHandler,
-        popupText : popupText,
-        changeText : changeText,
-        changeAge : changeAge,
-        popupAge : popupAge,
-        fullPopup : fullPopup,
-        introScroll : introScroll,
+        popup: popup,
+        introScroll: introScroll,
         tab: tab,
-        init: init
+        init: init,
+        selectChange: selectChange
     }
 })();
 
