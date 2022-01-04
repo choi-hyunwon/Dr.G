643e810d82ce063260782232c482236b7b8ab64d
taeyoon <Gotaeyoon007@gmail.com>
2022-01-04 17:38:46 +0900

    script 변경 진행중

--- Dr.G-mobile/src/js/common.js
+++ Dr.G-mobile/src/js/common.js
@@ -5,7 +5,7 @@
  v.0.2 태윤 : 모달 관련 스크립트 추가 -- 2021.12.21
  v.0.3 태윤 : 모달 리팩토링 -- 2021.12.22
  v.0.3 태윤 : 모달 관련 현재 주석처리 -- 2021.12.24
- v.0.4 태윤 : 모달 관련 현재 주석해제 및 모달 공통화 완료 -- 2021.01.
+ v.0.4 태윤 : 모달 관련 현재 주석해제 및 모달 공통화 완료 -- 2021.01.04
  * --------------------------------------------------------------------------
  */
 
@@ -53,40 +53,36 @@ front.common = (function () {
             });
         })
 
-        /* modal-select */
-        $('._toast-popup .select-list > .list').on('click', function (e) {
-            let textValueParent = $(this).parents('._toast-popup');
-            let textValueSelectPopup = $(this).parents('.select-popup').children('._toast-popup-open');
-            let textValue = $(this).find('span').text();
-
+        /*modal-select-close*/
+        $('._toast-popup .select-list > .list').on('click', function () {
+            let Popup = $(this).parents('._toast-popup');
 
             function hidePopup() {
-                textValueParent.addClass('hide');
+                Popup.addClass('hide');
                 setTimeout(() => {
-                    textValueParent.removeClass('fade show').css('display', 'none');
+                    Popup.removeClass('fade show').css('display', 'none');
                 }, 400);
-                /*클릭시 텍스트 변경*/
-            }
-
-            function ChangeText() {
-                textValueSelectPopup.find('span').text(textValue).css({
-                    'color': '#444444',
-                    'font-weight': 'bold'
-                });
             }
 
             if ($('._toast-popup .select-list > .list').hasClass("active")) {
                 $(this).siblings().removeClass("active");
                 $(this).addClass("active");
                 hidePopup();
-                ChangeText();
             } else {
                 $(this).addClass("active")
                 hidePopup();
-                ChangeText();
             }
 
+        })
 
+        /*text-change*/
+        $('._toast-popup-open').on('click', function () {
+            let btnText = $(this).find('span').text();
+
+            btnText.text(btnText).css({
+                'color': '#444444',
+                'font-weight': 'bold'
+            });
         })
     }
 
@@ -106,12 +102,9 @@ front.common = (function () {
         var lastScroll = 0;
         $('._full-popup > .popup-content').scroll(function () {
             var st = $(this).scrollTop();
-            // console.log(st);
             if (st === 0) {
-                // console.log('Down');
                 $('._popup-header').removeClass('header-white');
             } else {
-                // console.log('Up');
                 $('._popup-header').addClass('header-white');
             }
             lastScroll = st;
@@ -123,7 +116,6 @@ front.common = (function () {
         let popupClose = $('._toast-popup-close');
         let select = $('._toast-popup');
 
-
         popupOpen.on('click', function () {
             const idx = popupOpen.index(this);
             select.eq(idx).removeClass('hide').addClass('fade show').css('display', 'block');
@@ -131,6 +123,7 @@ front.common = (function () {
         popupClose.on('click', function () {
             select.removeClass('show').addClass('hide').css('display', 'none');
         });
+
     }
 
     return {
