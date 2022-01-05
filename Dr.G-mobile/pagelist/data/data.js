babccbe7fa9221d3b226484a98b6d4c1ec8f2360
bcde77 <bcde77@domfam.com>
2022-01-05 12:24:05 +0900

    서브메인작업중

--- Dr.G-mobile/pagelist/data/data.js
+++ Dr.G-mobile/pagelist/data/data.js
@@ -164,7 +164,7 @@ data.AI = {
 data.mentoring = {
     title: '1:1스킨 멘토링',
     list: [
-    /*    {
+        {
             stateClass: 'waiting',
             url: '스킨멘토링_서브메인',
             category: 'mentoring',
@@ -175,7 +175,7 @@ data.mentoring = {
             url: '스킨멘토링_서브메인_피부분석_후기전체보기',
             category: 'mentoring',
             page: '스킨멘토링_서브메인_피부분석_후기전체보기'
-        },*/
+        },
         {
             stateClass: 'complete',
             url: '스킨멘토링_스킨멘토링내역_목록',
