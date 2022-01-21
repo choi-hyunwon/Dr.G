997e172aecec1c76339c60b3a47fc0b07db11cf1
Juyeon-0919 <janielee0919@domfam.com>
2022-01-21 18:37:41 +0900

    * 1:1 스킨멘토링 script 수정하기

--- Dr.G-mobile/src/js/common.js
+++ Dr.G-mobile/src/js/common.js
@@ -188,42 +188,46 @@ front.common = (function () {
 
     var headerDetails = function () {
         if ($('.container').find('._static').length == false) {
-            // Hide header on scroll down
-            var didScroll;
-            var lastScrollTop = 0;
-            var delta = 5;
-            var headerDetail = $('.header-details');
-            var headerDetailHeight = headerDetail.outerHeight();
+            if($('.container').find('.step-heading').length == false) {
+                var didScroll;
+                var lastScrollTop = 0;
+                var delta = 5;
+                var headerDetail = $('.header-details');
+                var headerDetailHeight = headerDetail.outerHeight();
 
-            $(window).scroll(function(event){
-                didScroll = true;
-            });
+                $(window).scroll(function(event){
+                    didScroll = true;
+                });
 
-            setInterval(function() {
-                if (didScroll) {
-                    hasScrolled();
-                    didScroll = false;
-                }
-            }, 250);
-
-            function hasScrolled() {
-                var st = $(this).scrollTop();
-
-                // Make scroll more than delta
-                if(Math.abs(lastScrollTop - st) <= delta)
-                    return;
-
-                // If scrolled down and past the navbar, add class .nav-up.
-                if (st > lastScrollTop && st > headerDetailHeight){
-                    // Scroll Down
-                    headerDetail.removeClass('nav-down').addClass('nav-up');
-                } else {
-                    // Scroll Up
-                    if(st + $(window).height() < $(document).height()) {
-                        headerDetail.removeClass('nav-up').addClass('nav-down');
+                setInterval(function() {
+                    if (didScroll) {
+                        hasScrolled();
+                        didScroll = false;
                     }
+                }, 250);
+
+                function hasScrolled() {
+                    var st = $(this).scrollTop();
+
+                    // Make scroll more than delta
+                    if(Math.abs(lastScrollTop - st) <= delta)
+                        return;
+
+                    // If scrolled down and past the navbar, add class .nav-up.
+                    if (st > lastScrollTop && st > headerDetailHeight){
+                        // Scroll Down
+                        headerDetail.removeClass('nav-down').addClass('nav-up');
+                    } else {
+                        // Scroll Up
+                        if(st + $(window).height() < $(document).height()) {
+                            headerDetail.removeClass('nav-up').addClass('nav-down');
+                        }
+                    }
+                    lastScrollTop = st;
                 }
-                lastScrollTop = st;
+            }
+           else {
+                console.log('todo!')
             }
         }
 
