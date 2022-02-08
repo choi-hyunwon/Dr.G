a9ab8b81c828aa8069acff9bf8848e602570ce1d
Juyeon-0919 <janielee0919@domfam.com>
2022-02-08 09:20:14 +0900

    * 애니메이션 테스트

--- Dr.G-mobile/gulpfile.js
+++ Dr.G-mobile/gulpfile.js
@@ -46,6 +46,7 @@ function copyLibJs() {
         'src/js/lib/jquery.cloud9carousel.js',
         'src/js/lib/jquery.waypoints.js',
         'src/js/lib/lazyload.js',
+        'src/js/lib/lottie_svg.js',
     ];
 
     return gulp.src(sourceLib)
